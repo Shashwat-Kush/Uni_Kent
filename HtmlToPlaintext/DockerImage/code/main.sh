ba510b74d287039dfdb4091649531b86553db20f
Kaatil_Kabootar <124490948+Shashwat-Kush@users.noreply.github.com>
2024-05-21 12:58:34 +0530

    Add files via upload

---
+++ HtmlToPlaintext/DockerImage/code/main.sh
@@ -0,0 +1,8 @@
+!/bin/bash
+
+mkdir -p /ext/plaintext_policies
+python2 Preprocessor.py --input "/ext/html_policies" --output "/ext/plaintext_policies"
+
+
+
+#/Users/shashwatkushwaha/Desktop/KENT/HtmlToPlaintext/ext/html_policies
